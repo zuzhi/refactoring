3b71c7c6f3aa551002cdb4092aacedde8cee0378
zuzhi <476513451@qq.com>
2018-05-04 12:44:48 +0800

    Extract method

--- src/main/java/com/zuzhi/refactoring/chapter1/Customer.java
+++ src/main/java/com/zuzhi/refactoring/chapter1/Customer.java
@@ -32,13 +32,7 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental each : rentals) {
-            // add frequent renter points
-            frequentRenterPoints++;
-
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
