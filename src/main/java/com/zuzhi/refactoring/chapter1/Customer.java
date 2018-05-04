0fbaf58e82e13135d09bd9b7fecd902cc2233848
zuzhi <476513451@qq.com>
2018-05-04 11:06:58 +0800

    Move method

--- src/main/java/com/zuzhi/refactoring/chapter1/Customer.java
+++ src/main/java/com/zuzhi/refactoring/chapter1/Customer.java
@@ -33,8 +33,7 @@ public class Customer {
         String result = "Rental record for " + getName() + "\n";
         for (Rental each : rentals) {
             double thisAmount = 0;
-            thisAmount = amountFor(each);
-
+            thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -55,28 +54,4 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        // determine amounts for each line
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2) {
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHIRDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-            default:
-                break;
-        }
-        return result;
-    }
 }
