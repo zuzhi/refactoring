8db82f9d503356b1b60c79a126cf261a2182beea
zuzhi <476513451@qq.com>
2018-05-05 08:07:31 +0800

    Replacing the Conditional Logic on Price Code with Polymorphism

--- src/main/java/com/zuzhi/refactoring/chapter1/Movie.java
+++ src/main/java/com/zuzhi/refactoring/chapter1/Movie.java
@@ -31,4 +31,37 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        // determine amounts for each line
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHIRDREN:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
--- src/main/java/com/zuzhi/refactoring/chapter1/Rental.java
+++ src/main/java/com/zuzhi/refactoring/chapter1/Rental.java
@@ -25,35 +25,10 @@ public class Rental {
     }
 
     double getCharge() {
-        double result = 0;
-        // determine amounts for each line
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHIRDREN:
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-                break;
-            default:
-                break;
-        }
-        return result;
+        return movie.getCharge(daysRented);
     }
 
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
